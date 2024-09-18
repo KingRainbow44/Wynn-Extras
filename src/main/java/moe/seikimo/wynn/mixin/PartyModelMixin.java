0805627ac9f91f3f68fd983f1ee1e04e2caeb9c1
KingRainbow44 <kobedo11@gmail.com>
2024-09-17 20:16:50 -0400

    fix: Re-authenticate when player joins a party

--- src/main/java/moe/seikimo/wynn/mixin/PartyModelMixin.java
+++ src/main/java/moe/seikimo/wynn/mixin/PartyModelMixin.java
@@ -6,6 +6,7 @@ import com.wynntils.core.components.Models;
 import com.wynntils.core.components.Services;
 import com.wynntils.core.text.StyledText;
 import com.wynntils.models.players.PartyModel;
+import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -22,13 +23,29 @@ public abstract class PartyModelMixin extends Model {
 
     @Shadow(remap = false) @Final private static Pattern PARTY_JOIN_SELF;
 
+    @Shadow(remap = false) @Final private static Pattern PARTY_JOIN_OTHER;
+
     protected PartyModelMixin(List<Model> dependencies) {
         super(dependencies);
     }
 
     @Inject(method = "tryParsePartyMessages", at = @At("HEAD"), remap = false)
     public void onMessageReceived(StyledText styledText, CallbackInfoReturnable<Boolean> cir) {
-        if (styledText.matches(PARTY_CREATE_SELF) || styledText.matches(PARTY_JOIN_SELF)) {
+        var shouldAuth =
+                styledText.matches(PARTY_CREATE_SELF) ||
+                styledText.matches(PARTY_JOIN_SELF);
+
+        var matcher = styledText.getMatcher(PARTY_JOIN_OTHER);
+        if (matcher.matches() && !shouldAuth) {
+            var username = matcher.group(1);
+
+            var player = MinecraftClient.getInstance().player;
+            if (player != null) {
+                shouldAuth = player.getGameProfile().getName().equals(username);
+            }
+        }
+
+        if (shouldAuth) {
             WynntilsMod.info("Attempting to re-authenticate with Hades (party created/joined)");
 
             // Re-authenticate with the player server.
